264f6852858f3ad4223e12411772a1a303792d7a
Teodor Velikov <theo.velikov@gmail.com>
2020-06-24 12:09:27 -0700

    fixed rubocop issues

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -25,7 +25,7 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'tag_list and erb_contains_tag' do
-    assert_difference %w(ActsAsTaggableOn::Tag.count ActsAsTaggableOn::Tagging.count), 2 do
+    assert_difference %w[ActsAsTaggableOn::Tag.count ActsAsTaggableOn::Tagging.count], 2 do
       assert_difference 'Image.count', 1 do
         post images_path, params: { image: { url: 'https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2018/08/kitten-440379.jpg?h=c8d00152&itok=1fdekAh2', tag_list: 'Cat, Cute' } } # rubocop:disable Metrics/LineLength
         assert_redirected_to image_path(Image.last.id)
@@ -41,7 +41,7 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     end
   end
   test 'empty_tag_list' do
-    assert_difference %w(ActsAsTaggableOn::Tag.count ActsAsTaggableOn::Tagging.count), 0 do
+    assert_difference %w[ActsAsTaggableOn::Tag.count ActsAsTaggableOn::Tagging.count], 0 do
       assert_difference 'Image.count', 1 do
         post images_path, params: { image: { url: 'https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2018/08/kitten-440379.jpg?h=c8d00152&itok=1fdekAh2' } } # rubocop:disable Metrics/LineLength
         assert_redirected_to image_path(Image.last.id)
